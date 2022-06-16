f85d61a00b1aefe5185539f6bb86cfeb836b611b
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-06-16 02:22:15 -0300

    :sparkles: feat(index.js): Collect page elements and added functions "open/close"

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,23 @@
 import IndexFunctions from "../scripts/index.js";
+const openBtn = document.querySelector("button");
+const closeBtn = document.querySelector('[data-button="close"]');
+// console.log(openBtn,closeBtn)
+const modal = document.querySelector(".modal");
+
+openBtn.addEventListener("click", openModal);
+
+function openModal() {
+  modal.style.display = "grid";
+}
+
+closeBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    closeModal();
+  }
+});
